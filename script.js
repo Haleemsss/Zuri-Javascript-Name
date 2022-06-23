e4fe047fea5e973ae4fba8dae4af78af37591de2
Halimat umar <105776684+Haleemsss@users.noreply.github.com>
2022-06-23 09:21:17 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var FirstName = 'Halimat'
+var LastName = 'Umar'
+var Height = '5.5ft'
+var country = 'Nigeria'
+
+
+
+document.write('FirstName:' + ' ' + FirstName + '<br/>');
+document.write('LastName:' + ' ' + LastName + '<br/>');
+document.write('Height:' + ' ' + Height + '<br/>');
+document.write('Country:' + ' ' + country + '<br/>');
